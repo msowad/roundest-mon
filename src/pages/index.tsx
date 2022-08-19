66abb0fd2ed247061d1fab53a35a23809e1c95c0
Sowad <sowad.dev@gmail.com>
2022-08-19 16:35:00 +0600

    basic layout and styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,16 @@ interface Props {
 
 const Index: NextPage<Props> = () => {
   return (
-    <div className="bg-gray-800 h-screen w-screen flex flex-col justify-center items-center">
-      hello
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="text-2xl sm:text-3xl font-bold sm:font-black">
+        Which Pokémon is Rounder?
+      </div>
+      <div className="p-2" />
+      <div className="border-2 rounded p-8 flex justify-between items-center">
+        <div className="w-16 h-16 bg-red-800"></div>
+        <div className="p-8 text-3xl font-semibold italic">vs</div>
+        <div className="w-16 h-16 bg-green-800"></div>
+      </div>
     </div>
   );
 };
