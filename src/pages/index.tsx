c2d0fba438f472735dc66b92135515dab79f3d1c
Sowad <sowad.dev@gmail.com>
2022-08-19 16:19:17 +0600

    tailwindcss and bacis folder sctructure

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import { NextPage } from "next";
+
+interface Props {
+  //
+}
+
+const Index: NextPage<Props> = () => {
+  return (
+    <div className="bg-gray-800 h-screen w-screen flex flex-col justify-center items-center">
+      hello
+    </div>
+  );
+};
+
+export default Index;
